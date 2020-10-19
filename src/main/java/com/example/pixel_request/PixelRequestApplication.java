8059ddcb14c03ac043a87fb9710008eeda285b6e
binkul <jacekbinkul@gmail.com>
2020-10-19 19:23:16 +0200

    Initial commit

---
+++ src/main/java/com/example/pixel_request/PixelRequestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pixel_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelRequestApplication.class, args);
+    }
+
+}
